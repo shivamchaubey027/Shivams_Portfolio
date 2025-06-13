c52de5a206c440fcb37a3b0b21389f115b841bd7
Shivam Chaubey <shivamchaubey027@gmail.com>
2025-06-13 23:53:41 +0530

    Fix: Adjust INSTALLED_APPS order for Cloudinary

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',       
     'django.contrib.staticfiles',
+    'cloudinary',                
     'ckeditor',
     'ckeditor_uploader',
     'portfolio',
-    'cloudinary_storage',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
