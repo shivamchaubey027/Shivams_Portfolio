1c222fc2b16b61659211299caf2f213af9bd50a4
Shivam Chaubey <shivamchaubey027@gmail.com>
2025-06-13 18:06:42 +0530

    Fix typo in settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -10,7 +10,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY= config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
