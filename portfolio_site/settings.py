d368babef31475276554a1285394d981c3624d6b
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-07-19 22:24:36 +0530

    Correct INSTALLED_APPS order

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',       
-    'django.contrib.staticfiles',
-    'cloudinary',                
+    'django.contrib.staticfiles',  
+
+    'cloudinary_storage',
+    'cloudinary',
     'ckeditor',
     'ckeditor_uploader',
+
     'portfolio',
 ]
 
