ea1bbb6cb00bf2aff75653a1af26da7b5f3a350d
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-09-27 12:23:14 +0530

    New domains

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('metrics/', include('django_prometheus.urls')), # Added for Prometheus metrics
     path('', include('portfolio.urls')),
 ]
 
